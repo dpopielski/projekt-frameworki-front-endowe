f03cb9f7604bb7bf50a89308f8e595cdd8371d2c
Damian Popielski <damianpopielski.dp@gmail.com>
2021-05-19 00:21:47 +0200

    carousel components

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,19 +1,25 @@
 import { FC } from "react";
 import styled from "styled-components";
 import WorkspaceCard from "./WorkspaceCard";
+import Carousel from 'react-elastic-carousel'
 
 const Wrapper = styled.div`
   display: flex;
-  gap: 20px;
+  justify-content: flex-start;
+  gap: 10px;
 `;
 
 export const Workspace: FC = () => {
-
   return (
     <Wrapper>
-      <WorkspaceCard />
-      <WorkspaceCard /> 
-      <WorkspaceCard /> 
+      <Carousel isRTL={false} itemsToShow={4} showArrows={false} itemPosition={"START"} pagination={false}>
+        <WorkspaceCard>1</WorkspaceCard>
+        <WorkspaceCard>2</WorkspaceCard>
+        <WorkspaceCard>3</WorkspaceCard>
+        <WorkspaceCard>4</WorkspaceCard>
+        <WorkspaceCard>5</WorkspaceCard>
+        <WorkspaceCard>6</WorkspaceCard>
+      </Carousel>
     </Wrapper>
   );
 };
--- src/components/Workspace/WorkspaceCard.tsx
+++ src/components/Workspace/WorkspaceCard.tsx
@@ -2,18 +2,82 @@ import { FC } from "react";
 import styled from "styled-components";
 
 const Card = styled.div`
-  height: 200px;
-  width: 300px;
+position: relative;
+  height: 250px;
+  width: 350px;
   border-radius: 5px;
-  background-color: black;
+  background-color: #fff;
+  overflow: hidden;
 `;
 
-const CardInner = styled.div``;
+const CardImg = styled.div`
+  background-image: url("./assets/images/img1.jpg");
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  background-size: cover;
+  height: 45%;
+`;
+
+const Content = styled.div`
+height: 55%;
+  padding: 10px;
+
+  div {
+    height: 60%;
+
+    p { 
+      margin-left: 35%;
+    }
+  }
+
+  span { 
+    display: flex;
+    gap: 15px;
+  }
+`;
+
+const CardInnerImg = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  left: 10px;
+  top: 75px;
+  height: 100px;
+  width: 100px;
+  border-radius: 5px;
+  background-color: #fff;
+    --tw-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
+    var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+
+    img { 
+      width: 50px;
+    }
+`;
 
 export const WorkspaceCard: FC = () => {
   return (
     <>
-      <Card></Card>
+      <Card>
+        <CardInnerImg><img src="./assets/icons/entities.png" alt="" /></CardInnerImg>
+        <CardImg></CardImg>
+        <Content>
+          <div>
+            <p>Client contract</p>
+          </div>
+          
+          <span>
+            <img src="./assets/icons/people.png" alt="" />
+            Contract -
+            <img src="./assets/icons/people.png" alt="" />
+            150 users
+          </span>
+
+          <span>Last update 2 days ago</span>
+
+        </Content>
+      </Card>
     </>
   );
 };
