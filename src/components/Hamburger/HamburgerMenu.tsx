28e8f79e19f0aeed0865607abdc360b25377f469
Damian Popielski <damianpopielski.dp@gmail.com>
2021-06-26 12:21:40 +0200

    some updates

--- src/components/Hamburger/HamburgerMenu.tsx
+++ src/components/Hamburger/HamburgerMenu.tsx
@@ -1,73 +1,72 @@
-import { FC, useState } from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import LeftMenu from "../LeftMenu/LeftMenu";
+import { FC, useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const PlatformData = [
   {
-    title: "Home",
-    path: "/",
+    title: 'Home',
+    path: '/',
     icone: `./assets/icons/house.svg`,
   },
   {
-    title: "Publications",
-    path: "/404",
+    title: 'Publications',
+    path: '/404',
     icone: `./assets/icons/publications.svg`,
   },
   {
-    title: "People",
-    path: "/404",
+    title: 'People',
+    path: '/404',
     icone: `./assets/icons/people.svg`,
   },
   {
-    title: "Entities",
-    path: "/404",
+    title: 'Entities',
+    path: '/404',
     icone: `./assets/icons/entities.svg`,
   },
   {
-    title: "Administration",
-    path: "/404",
+    title: 'Administration',
+    path: '/404',
     icone: `./assets/icons/administration.svg`,
   },
 ];
 
 const WorkspacesData = [
   {
-    title: "Client contract",
-    path: "#",
+    title: 'Client contract',
+    path: '#',
     icone: `./assets/icons/publications.svg`,
   },
   {
-    title: "Supplier contract",
-    path: "#",
+    title: 'Supplier contract',
+    path: '#',
     icone: `./assets/icons/publications.svg`,
   },
   {
-    title: "Corporate",
-    path: "#",
+    title: 'Corporate',
+    path: '#',
     icone: `./assets/icons/entities2.svg`,
   },
   {
-    title: "Group Norms",
-    path: "#",
+    title: 'Group Norms',
+    path: '#',
     icone: `./assets/icons/publications.svg`,
   },
   {
-    title: "Real estate contracts",
-    path: "#",
+    title: 'Real estate contracts',
+    path: '#',
     icone: `./assets/icons/publications.svg`,
   },
 ];
 
 const AccountData = [
   {
-    title: "Privacy",
-    path: "#",
+    title: 'Privacy',
+    path: '#',
     icone: `./assets/icons/privacy.svg`,
   },
   {
-    title: "Settings",
-    path: "#",
+    title: 'Settings',
+    path: '#',
     icone: `./assets/icons/settings.svg`,
   },
 ];
@@ -85,7 +84,7 @@ const StyledMenu = styled.nav<Props>`
   display: flex;
   flex-direction: column;
   background: #f5f5f5;
-  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
+  transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(-100%)')};
   height: 100vh;
   text-align: left;
   padding: 4rem 1rem;
@@ -94,7 +93,7 @@ const StyledMenu = styled.nav<Props>`
   left: 0;
   width: 33.333333%;
   transition: transform 0.3s ease-in-out;
-  box-shadow: ${({ open }) => (open ? "0px 1px 10px #999;" : "none")};
+  box-shadow: ${({ open }) => (open ? '0px 1px 10px #999;' : 'none')};
 
   @media (max-width: 576px) {
     width: 100%;
@@ -106,75 +105,73 @@ const StyledMenu = styled.nav<Props>`
 `;
 
 export const HamburgerMenu: FC<HamburgerProps> = ({ open }) => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
   return (
     <StyledMenu open={open}>
-      <div className="flex align-center p-1.5 w-full">
+      <div className='flex align-center p-1.5 w-full'>
         <input
-          className="text-xs p-1.5 border-2 border-solid border-gray-100 rounded-sm w-full"
-          placeholder="Filter..."
-          type="text"
+          className='text-xs p-1.5 border-2 border-solid border-gray-100 rounded-sm w-full'
+          placeholder='Filter...'
+          type='text'
           onChange={(e) => setSearch(e.target.value.toLowerCase())}
         />
       </div>
 
-      <div className="mt-2">
-        <span className="text-xs px-2">Platform</span>
+      <div className='mt-2'>
+        <span className='text-xs px-2'>Platform</span>
         {PlatformData.filter((elem) =>
           elem.title.toLowerCase().includes(search)
         ).map((elem, index) => (
           <Link to={elem.path} key={index}>
-            <ul
-            className="flex justify-start items-center p-2 gap-4 w-full hover:bg-gray-300 cursor-pointer"
-            >
-              <img src={elem.icone} alt="" />
-              <li className="text-sm">{elem.title}</li>
+            <ul className='flex justify-start items-center p-2 gap-4 w-full hover:bg-gray-300 cursor-pointer'>
+              <img src={elem.icone} alt='' />
+              <li className='text-sm'>{elem.title}</li>
             </ul>
           </Link>
         ))}
       </div>
 
-      <div className="">
-        <span className="text-xs px-2">Workspace</span>
+      <div className=''>
+        <span className='text-xs px-2'>Workspace</span>
         {WorkspacesData.filter((elem) =>
           elem.title.toLowerCase().includes(search)
         ).map((elem, index) => (
           <Link to={elem.path} key={index}>
-            <ul
-            className="flex justify-start items-center p-2 gap-4 w-full hover:bg-gray-300 cursor-pointer"
-            >
-            <img src={elem.icone} alt="" />
-            <li className="text-sm">{elem.title}</li>
-          </ul>
+            <ul className='flex justify-start items-center p-2 gap-4 w-full hover:bg-gray-300 cursor-pointer'>
+              <img src={elem.icone} alt='' />
+              <li className='text-sm'>{elem.title}</li>
+            </ul>
           </Link>
         ))}
       </div>
 
-      <div className="border-b-2">
-          <span className="text-xs px-2">Account</span>
-          <div className="flex justify-start items-center p-2 gap-4 w-full">
-            <img className="h-5 w-5" src={"./assets/profile.svg"} alt="" />
-            <div className="flex flex-col">
-              <span className="flex items-center text-sm"> Jeanne-Marie Li</span>
-              <Link to="/profile" className="text-xs">See profile</Link>
-            </div>
+      <div className='border-b-2'>
+        <span className='text-xs px-2'>Account</span>
+        <div className='flex justify-start items-center p-2 gap-4 w-full'>
+          <img className='h-5 w-5' src={'./assets/profile.svg'} alt='' />
+          <div className='flex flex-col'>
+            <span className='flex items-center text-sm'> Jeanne-Marie Li</span>
+            <Link to='/profile' className='text-xs'>
+              See profile
+            </Link>
           </div>
-          {AccountData.filter((elem) =>
-            elem.title.toLowerCase().includes(search)
-          ).map((elem, index) => (
-            <ul
-              key={index}
-              className="flex justify-start items-center p-2 gap-4 w-full cursor-pointer hover:bg-gray-300"
-            >
-              <img className="h-5 w-5" src={elem.icone} alt="" />
-              <li className="flex items-center text-sm">{elem.title}</li>
-            </ul>
-          ))}
         </div>
-        <div className="flex justify-center items-center p-2 gap-3 w-full">
-          <img className="h-5 w-5" src={"./assets/icons/logout.svg"} alt="" />
-          <span className="flex items-center text-sm">Logout</span>
-        </div>
+        {AccountData.filter((elem) =>
+          elem.title.toLowerCase().includes(search)
+        ).map((elem, index) => (
+          <ul
+            key={index}
+            className='flex justify-start items-center p-2 gap-4 w-full cursor-pointer hover:bg-gray-300'
+          >
+            <img className='h-5 w-5' src={elem.icone} alt='' />
+            <li className='flex items-center text-sm'>{elem.title}</li>
+          </ul>
+        ))}
+      </div>
+      <div className='flex justify-center items-center p-2 gap-3 w-full'>
+        <img className='h-5 w-5' src={'./assets/icons/logout.svg'} alt='' />
+        <span className='flex items-center text-sm'>Logout</span>
+      </div>
     </StyledMenu>
   );
 };
--- src/components/TopBar/ExpandedMenu.tsx
+++ src/components/TopBar/ExpandedMenu.tsx
@@ -1,142 +1,150 @@
-import { FC, useState } from "react";
-import { Link } from "react-router-dom";
+import { FC, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const PlatformData = [
   {
-    title: "Home",
-    path: "/",
+    title: 'Home',
+    path: '/',
     icone: `./assets/icons/house.svg`,
   },
   {
-    title: "Publications",
-    path: "/404",
+    title: 'Publications',
+    path: '/publications',
     icone: `./assets/icons/publications.svg`,
   },
   {
-    title: "People",
-    path: "/404",
+    title: 'People',
+    path: '/people',
     icone: `./assets/icons/people.svg`,
   },
   {
-    title: "Entities",
-    path: "/404",
+    title: 'Entities',
+    path: '/entities',
     icone: `./assets/icons/entities.svg`,
   },
   {
-    title: "Administration",
-    path: "/404",
+    title: 'Administration',
+    path: '/administration',
     icone: `./assets/icons/administration.svg`,
   },
 ];
 
 const WorkspacesData = [
   {
-    title: "Client contract",
-    path: "#",
+    title: 'Client contract',
+    path: '/client-contract',
     icone: `./assets/icons/publications.svg`,
   },
   {
-    title: "Supplier contract",
-    path: "#",
+    title: 'Supplier contract',
+    path: '/supplier-contract',
     icone: `./assets/icons/publications.svg`,
   },
   {
-    title: "Corporate",
-    path: "#",
+    title: 'Corporate',
+    path: '/corporate',
     icone: `./assets/icons/entities2.svg`,
   },
   {
-    title: "Group Norms",
-    path: "#",
+    title: 'Group Norms',
+    path: 'group-norms',
     icone: `./assets/icons/publications.svg`,
   },
   {
-    title: "Real estate contracts",
-    path: "#",
+    title: 'Real estate contracts',
+    path: 'real-estate-contracts',
     icone: `./assets/icons/publications.svg`,
   },
 ];
 
 const AccountData = [
   {
-    title: "Privacy",
-    path: "#",
+    title: 'Privacy',
+    path: '/privacy',
     icone: `./assets/icons/privacy.svg`,
   },
   {
-    title: "Settings",
-    path: "#",
+    title: 'Settings',
+    path: '/settings',
     icone: `./assets/icons/settings.svg`,
   },
 ];
 
 export const ExpandedMenu: FC = () => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
 
   return (
-      <nav
-        className="absolute flex flex-col w-full top-9 cursor-default bg-white border-t-2"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <div className="flex items-center p-1.5 w-full">
-          <input
-            className="text-xs p-1.5 border-2 border-solid border-gray-100 rounded-sm w-full"
-            placeholder="Filter..."
-            type="text"
-            onChange={(e) => setSearch(e.target.value.toLowerCase())}
-          />
-        </div>
-        <div className="border-b-2">
-          <span className="text-xs px-2">Platform</span>
-          {PlatformData.filter((elem) =>
-            elem.title.toLowerCase().includes(search)
-          ).map((elem, index) => (
-            <Link to={elem.path} key={index} className="">
-              <ul className="flex justify-start items-center p-2 gap-4 hover:bg-gray-300 cursor-pointer">
-                <img className="h-5 w-5" src={elem.icone} alt="" />
-                <li className="flex items-center text-sm">{elem.title}</li>
-              </ul>
+    <nav
+      className='absolute flex flex-col w-full top-9 cursor-default bg-white border-t-2'
+      onClick={(e) => e.stopPropagation()}
+    >
+      <div className='flex items-center p-1.5 w-full'>
+        <input
+          className='text-xs p-1.5 border-2 border-solid border-gray-100 rounded-sm w-full'
+          placeholder='Filter...'
+          type='text'
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        />
+      </div>
+      <div className='border-b-2'>
+        <span className='text-xs px-2'>Platform</span>
+        {PlatformData.filter((elem) =>
+          elem.title.toLowerCase().includes(search)
+        ).map((elem, index) => (
+          <Link to={elem.path} key={index} className=''>
+            <ul className='flex justify-start items-center p-2 gap-4 hover:bg-gray-300 cursor-pointer'>
+              <img className='h-5 w-5' src={elem.icone} alt='' />
+              <li className='flex items-center text-sm'>{elem.title}</li>
+            </ul>
+          </Link>
+        ))}
+        <span className='text-xs px-2'>Workspace</span>
+        {WorkspacesData.filter((elem) =>
+          elem.title.toLowerCase().includes(search)
+        ).map((elem, index) => (
+          <Link to={elem.path} key={index}>
+            <ul
+              key={index}
+              className='flex justify-start items-center p-2 gap-4 cursor-pointer w-full hover:bg-gray-300'
+            >
+              <img className='h-5 w-5' src={elem.icone} alt='' />
+              <li className='flex items-center text-sm'>{elem.title}</li>
+            </ul>
+          </Link>
+        ))}
+      </div>
+      <div className='border-b-2'>
+        <span className='text-xs px-2'>Account</span>
+        <div className='flex justify-start items-center p-2 gap-4 w-full'>
+          <img className='h-5 w-5' src={'./assets/profile.svg'} alt='' />
+          <div className='flex flex-col'>
+            <span className='flex items-center text-sm'> Jeanne-Marie Li</span>
+            <Link to='/profile' className='text-xs '>
+              See profile
             </Link>
-          ))}
-          <span className="text-xs px-2">Workspace</span>
-          {WorkspacesData.filter((elem) =>
-            elem.title.toLowerCase().includes(search)
-          ).map((elem, index) => (
-            <ul
-              key={index}
-              className="flex justify-start items-center p-2 gap-4 cursor-pointer w-full hover:bg-gray-300"
-            >
-              <img className="h-5 w-5" src={elem.icone} alt="" />
-              <li className="flex items-center text-sm">{elem.title}</li>
-            </ul>
-          ))}
-        </div>
-        <div className="border-b-2">
-          <span className="text-xs px-2">Account</span>
-          <div className="flex justify-start items-center p-2 gap-4 w-full">
-            <img className="h-5 w-5" src={"./assets/profile.svg"} alt="" />
-            <div className="flex flex-col">
-              <span className="flex items-center text-sm"> Jeanne-Marie Li</span>
-              <Link to="/profile" className="text-xs">See profile</Link>
-            </div>
           </div>
-          {AccountData.filter((elem) =>
-            elem.title.toLowerCase().includes(search)
-          ).map((elem, index) => (
+        </div>
+        {AccountData.filter((elem) =>
+          elem.title.toLowerCase().includes(search)
+        ).map((elem, index) => (
+          <Link to={elem.path} key={index}>
             <ul
               key={index}
-              className="flex justify-start items-center p-2 gap-4 w-full cursor-pointer hover:bg-gray-300"
+              className='flex justify-start items-center p-2 gap-4 w-full cursor-pointer hover:bg-gray-300'
             >
-              <img className="h-5 w-5" src={elem.icone} alt="" />
-              <li className="flex items-center text-sm">{elem.title}</li>
+              <img className='h-5 w-5' src={elem.icone} alt='' />
+              <li className='flex items-center text-sm'>{elem.title}</li>
             </ul>
-          ))}
+          </Link>
+        ))}
+      </div>
+      <Link to='/'>
+        <div className='flex justify-center items-center p-2 gap-3 w-full hover:bg-gray-300'>
+          <img className='h-5 w-5' src={'./assets/icons/logout.svg'} alt='' />
+          <span className='flex items-center text-sm'>Logout</span>
         </div>
-        <div className="flex justify-center items-center p-2 gap-3 w-full">
-          <img className="h-5 w-5" src={"./assets/icons/logout.svg"} alt="" />
-          <span className="flex items-center text-sm">Logout</span>
-        </div>
-      </nav>
+      </Link>
+    </nav>
   );
 };
 export default ExpandedMenu;
